6c7c4176daa61e653a8e61c7bb2f004f1dd4c6f5
Dasha Kurilova <kurilovadk@gmail.com>
2021-01-30 13:05:46 +0300

    Created the project

---
+++ PryanikyTest/PryanikyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PryanikyTest
+//
+//  Created by Дарья on 30.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
