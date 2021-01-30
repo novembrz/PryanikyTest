d5431b41e2751080a27aa7498d58c5523814b543
Dasha Kurilova <kurilovadk@gmail.com>
2021-01-30 13:58:22 +0300

    Added a podfile and the Alamofire

---
+++ PryanikyTest/PryanikyTest/Model/JSONModel.swift
@@ -0,0 +1,12 @@
+//
+//  JSONModel.swift
+//  PryanikyTest
+//
+//  Created by Дарья on 30.01.2021.
+//
+
+import Foundation
+
+struct JSONModel: Decodable {
+    
+}
---
+++ PryanikyTest/PryanikyTest/Service/NetworkService.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkService.swift
+//  PryanikyTest
+//
+//  Created by Дарья on 30.01.2021.
+//
+
+import Foundation
+
+struct NetworkService {
+    
+}
